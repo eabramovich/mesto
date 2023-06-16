135a0734990e13b93fbea516101504f61d4b6e91
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2023-06-16 17:00:38 +0300

    Added popup

---
+++ script.js
@@ -0,0 +1,40 @@
+let editButton = document.querySelector('.profile__edit-button');
+let closeButtton = document.querySelector('.edit-form__close-button');
+let formElement = document.querySelector('.edit-form');
+
+function openEditForm() {
+    let editForm = document.querySelector('.popup');
+    editForm.classList.add('popup_opened');
+    // Находим поля формы в DOM
+    let nameInput = document.querySelector('.edit-form__name');
+    let jobInput = document.querySelector('.edit-form__profession');
+    // Узнаем текущее значение данных профиля
+    let profileName = document.querySelector('.profile__info-name').textContent;
+    let profileProfession = document.querySelector('.profile__info-profession').textContent;
+    // Заполняем поля формы
+    nameInput.value = profileName;
+    jobInput.value = profileProfession;
+}
+
+function closeEditForm() {
+    let editForm = document.querySelector('.popup');
+    editForm.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Находим поля формы в DOM
+    let nameInput = document.querySelector('.edit-form__name');
+    let jobInput = document.querySelector('.edit-form__profession');
+    let name = nameInput.value; // Получение значения поля name
+    let profession = jobInput.value; // Получение значения поля profession
+    let profileInfoName = document.querySelector('.profile__info-name');
+    let profileInfoProfession = document.querySelector('.profile__info-profession');
+    profileInfoName.textContent = name;
+    profileInfoProfession.textContent = profession;
+    closeEditForm();
+}
+
+editButton.addEventListener('click', openEditForm);
+closeButtton.addEventListener('click', closeEditForm);
+formElement.addEventListener('submit', handleFormSubmit);
